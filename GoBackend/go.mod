7bc438e3b14da1593286e13ec9815f4e97df1733
Syssos <paral.cody98@gmail.com>
2021-07-06 22:37:40 -0600

    Initial push

---
+++ GoBackend/go.mod
@@ -0,0 +1,5 @@
+module github.com/Syssos/goact
+
+go 1.16
+
+require github.com/gorilla/websocket v1.4.2 // indirect
---
+++ frontend/src/App.js
@@ -0,0 +1,47 @@
+// App.js
+import React, { Component } from "react";
+import "./App.css";
+import { connect, sendMsg } from "./api";
+
+// Import our new component from it's relative path
+import ChatHistory from './components/ChatHistory/ChatHistory';
+import ChatInput from './components/ChatInput/ChatInput';
+import Header from './components/Header/Header';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatHistory: []
+    }
+  }
+
+  componentDidMount() {
+    connect((msg) => {
+      console.log("New Message")
+      this.setState(prevState => ({
+        chatHistory: [...this.state.chatHistory, msg]
+      }))
+      console.log(this.state);
+    });
+  }
+
+  send(event) {
+    if(event.keyCode === 13) {
+      sendMsg(event.target.value);
+      event.target.value = "";
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <ChatHistory chatHistory={this.state.chatHistory} />
+        <ChatInput send={this.send} />
+      </div>
+    );
+  }
+}
+
+export default App;
