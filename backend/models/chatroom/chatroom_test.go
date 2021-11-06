615c12ca710c272665ebc625de6d20f6e7bd78c0
Syssos <paral.cody98@gmail.com>
2021-11-06 11:33:36 -0700

    Ensured enough material to validate travis functionality was added

---
+++ backend/models/chatroom/chatroom_test.go
@@ -0,0 +1,21 @@
+package chatroom
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestChatroom(t *testing.T) {
+	t.Run("Testing New Chatroom returns empty Room", func(t *testing.T) {
+		// This test is used to ensure that the start method doesn't get removed by mistake as it is 
+		// responsible for the chatrooms users communication and is a vitial component in the app
+		newroom := NewRoom()
+
+		st := reflect.TypeOf(newroom)
+		_, ok := st.MethodByName("Start")
+		if !ok {
+			t.Error("No start Method exists")
+		}
+	})
+
+}
