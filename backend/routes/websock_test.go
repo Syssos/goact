14a3978ab538d2dd62368d73786086331c9a22d5
Syssos <paral.cody98@gmail.com>
2021-11-14 13:44:35 -0700

    finalized chatroom tests

--- backend/routes/websock_test.go
+++ backend/routes/websock_test.go
@@ -1,9 +1,11 @@
 package routes
 
 import (
+    "fmt"
     "strings"
     "testing"
     "net/http"
+    "encoding/json"
     "net/http/httptest"
 
     "github.com/gorilla/websocket"
@@ -138,59 +140,74 @@ func TestChatroomPoolSize(t *testing.T) {
     })
 }
 
-// func TestChatroomMSGBroadcast(t *testing.T) {
-//     t.Run("Checking msg broadcasting", func (t *testing.T){
-//         newroom := chatroom.NewRoom()
-//         go newroom.Start()
+func TestChatroomMSGBroadcast(t *testing.T) {
+    t.Run("Checking msg broadcasting", func (t *testing.T){
+        newroom := chatroom.NewRoom()
+        go newroom.Start()
         
-//         s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-//             cookie := http.Cookie{Name: "token", Value: GenerateTokenString()}
-//             r.AddCookie(&cookie)
+        s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+            cookie := http.Cookie{Name: "token", Value: GenerateTokenString()}
+            r.AddCookie(&cookie)
 
-//             serveWs(newroom, w, r)
-//         }))
+            serveWs(newroom, w, r)
+        }))
 
-//         defer s.Close()
+        // Convert http://127.0.0.1 to ws://127.0.0.
+        u := "ws" + strings.TrimPrefix(s.URL, "http")
 
-//         // Convert http://127.0.0.1 to ws://127.0.0.
-//         u := "ws" + strings.TrimPrefix(s.URL, "http")
+        // User1
+        ws, _, err := websocket.DefaultDialer.Dial(u, nil)
+        if err != nil {
+            t.Fatalf("%v", err)
+        }
 
-//         // User1
-//         ws, _, err := websocket.DefaultDialer.Dial(u, nil)
-//         if err != nil {
-//             t.Fatalf("%v", err)
-//         }
-//         defer ws.Close()
+        // User2
+        ws2, _, err := websocket.DefaultDialer.Dial(u, nil)
+        if err != nil {
+            t.Fatalf("%v", err)
+        }
 
-//         // User2
-//         ws2, _, err := websocket.DefaultDialer.Dial(u, nil)
-//         if err != nil {
-//             t.Fatalf("%v", err)
-//         }
-//         defer ws2.Close()
+        if len(newroom.Clients) != 2 {
+            t.Error("The clients were not added to chat pool")
+        }
 
-//         if len(newroom.Clients) != 2 {
-//             t.Error("The clients were not added to chat pool")
-//         }
-
-//         // Send message to server, read response and check to see if it's what we expect.
-//         for i := 0; i < 10; i++ {
-//             if err := ws.WriteMessage(websocket.TextMessage, []byte("hello")); err != nil {
-//                 t.Fatalf("%v", err)
-//             }
+        responseList := []string{}
+        // Send message to server, read response and check to see if it's what we expect.
+        for i := 0; i < 10; i++ {
+            if err := ws.WriteMessage(websocket.TextMessage, []byte("hello")); err != nil {
+                t.Fatalf("%v", err)
+            }
             
-//             _, p, err := ws.ReadMessage()
-//             if err != nil {
-//                 t.Fatalf("%v", err)
-//             }
+            _, p, err := ws2.ReadMessage()
+            if err != nil {
+                t.Fatalf("%v", err)
+            }
+            
+            var msg chatroom.Message
+            if err := json.Unmarshal(p, &msg); err != nil {
+                t.Errorf("Couldn't serialize mseeage data: %v", err)
+            }
 
-//             checkStr := "{\"type\":1,\"user\":\"TestUser1\",\"body\":\"User Joined\"}"
-//             if string(p) != checkStr {
-//                 t.Fatalf("bad message")
-//             }
-//         }
-//     })
-// }
+            // Account for message from user joining
+            joinStr := "User Joined"
+            helloStr := "hello"
+            
+            if msg.Body != helloStr {
+                if msg.Body != joinStr {
+                    str := fmt.Sprintf("MSG Body: |%v| joinStr: |%v| helloStr: |%v|\n", msg.Body, joinStr, helloStr)
+                    t.Errorf("bad message: %v", str)
+                }
+            }
+            responseList = append(responseList, msg.Body)
+        }
+        if len(responseList) == 9 {
+            t.Error("Incorrect amount of messages sent")
+        }
+        ws.Close()
+        ws2.Close()
+        s.Close()
+    })
+}
 
 func echo(w http.ResponseWriter, r *http.Request) {
     c, err := Upgrade(w, r)
