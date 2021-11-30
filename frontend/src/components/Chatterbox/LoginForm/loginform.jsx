4a4e16ce46109f242fe25b2dfc8728a3f4d0fe3e
Syssos <paral.cody98@gmail.com>
2021-11-29 20:47:02 -0700

    Made changes to have sign in contained in component

--- frontend/src/components/Chatterbox/LoginForm/loginform.jsx
+++ frontend/src/components/Chatterbox/LoginForm/loginform.jsx
@@ -28,7 +28,7 @@ const LoginForm = () => {
 	return (
 		<div className="wrapper">
 			<div className="form">
-				<h1 className="title">ChatterBot</h1>
+				<h1 className="title">Chatterbox</h1>
 				<form onSubmit={handleSubmit}>
 					<input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
 					<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
